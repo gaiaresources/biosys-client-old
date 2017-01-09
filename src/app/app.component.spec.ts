dd1ed93e0a3c6da441605b4471a0f968fcddf7e7
serge <serge@gaiaresources.com.au>
2017-01-09 13:48:31 +0800

    Code formatting

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,54 +1,43 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { APP_BASE_HREF } from '@angular/common';
-
-import {
-  async
-} from '@angular/core/testing';
-import {
-  Route
-} from '@angular/router';
-import {
-  RouterTestingModule
-} from '@angular/router/testing';
+import { Route } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-//import { HomeComponent } from './pages/home/home.component';
+
 export function main() {
 
-  describe('App component', () => {
+    describe('App component', () => {
 
-    let config: Route[] = [
-    ];
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        imports: [FormsModule, RouterTestingModule.withRoutes(config)],
-        declarations: [TestComponent,
-          AppComponent,
-         ],
-        providers: [
-          { provide: APP_BASE_HREF, useValue: '/' }
-        ]
-      });
+        let config: Route[] = [];
+        beforeEach(() => {
+            TestBed.configureTestingModule({
+                imports: [FormsModule, RouterTestingModule.withRoutes(config)],
+                declarations: [TestComponent, AppComponent],
+                providers: [
+                    {provide: APP_BASE_HREF, useValue: '/'}
+                ]
+            });
+        });
+
+        it('should build without a problem',
+            async(() => {
+                TestBed
+                    .compileComponents()
+                    .then(() => {
+                        let fixture = TestBed.createComponent(TestComponent);
+                        let compiled = fixture.nativeElement;
+
+                        expect(compiled).toBeTruthy();
+                    });
+            }));
     });
-
-    it('should build without a problem',
-      async(() => {
-        TestBed
-          .compileComponents()
-          .then(() => {
-            let fixture = TestBed.createComponent(TestComponent);
-            let compiled = fixture.nativeElement;
-
-            expect(compiled).toBeTruthy();
-          });
-      }));
-  });
 }
 
 @Component({
-  selector: 'test-cmp',
-  template: '<biosys-app></biosys-app>'
+    selector: 'test-cmp',
+    template: '<biosys-app></biosys-app>'
 })
 
 class TestComponent {
